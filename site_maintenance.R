2eca50e5cceb4bd3e6750d3a06cc7f103b8a205b
gardenberg <eivind.hageberg@gmail.com>
2023-11-13 22:29:05 +0100

    initial commit
    first commit

---
+++ site_maintenance.R
@@ -0,0 +1,8 @@
+#script for enkelt vedlikehold av site
+
+#ressurser
+#hovedside hos Posit: https://rstudio.github.io/distill/
+#getting started-guiden fra samme nettsted https://rstudio.github.io/distill/blog.html#getting-started
+#enkel guide: https://themockup.blog/posts/2020-08-01-building-a-blog-with-distill/
+
+
