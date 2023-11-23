3f46a660dc0e9f47288c678cf52c91682f63583e
gardenberg <eivind.hageberg@gmail.com>
2023-11-23 22:07:42 +0100

    add old posts
    oppdatert med de siste gamle postene

--- site_maintenance.R
+++ site_maintenance.R
@@ -6,10 +6,10 @@
 #Æ, ø, å kan ikke brukes i tittelen her, men kan brukes i selve artikkelen etterpå
 #mulig det skyldes slug, men lettere å la den settes automatisk
 distill::create_post(
-  title = "Hvor like er to variabler",
+  title = "Dashboard for Age of Empires 2",
   collection = "posts",
   author = "auto",
-  date = as.Date("2019-09-17")
+  date = as.Date("2022-04-09")
   )
 
 #skriv eller rediger posten
