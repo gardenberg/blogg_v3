bb235c2a1323c28377806a5a1c95fccecc5d4f46
Eivind Hageberg <eivind.hageberg@gmail.com>
2024-01-06 17:30:38 +0100

    ny post

--- site_maintenance.R
+++ site_maintenance.R
@@ -6,10 +6,10 @@
 #Æ, ø, å kan ikke brukes i tittelen her, men kan brukes i selve artikkelen etterpå
 #mulig det skyldes slug, men lettere å la den settes automatisk
 distill::create_post(
-  title = "kartlaging i R",
+  title = "apparent_temperature",
   collection = "posts",
   author = "auto",
-  date = as.Date("2023-12-03")
+  date = as.Date("2024-01-06")
   )
 
 #skriv eller rediger posten
